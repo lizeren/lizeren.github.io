0d961de5ba5c2e65c9bd5d899b992f96ac8bb819
Zeren Li <li3103@purdue.edu>
2024-12-18 01:32:41 -0500

    add button functiality

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -97,3 +97,21 @@ $(document).ready(function(){
   });
 
 });
+document.addEventListener('DOMContentLoaded', function() {
+  Prism.plugins.toolbar.registerButton('copy-to-clipboard', function(env) {
+    let button = document.createElement('button');
+    button.textContent = 'Copy';
+    
+    button.addEventListener('click', function() {
+      let code = env.element.textContent;
+      navigator.clipboard.writeText(code).then(function() {
+        button.textContent = 'Copied!';
+        setTimeout(function() {
+          button.textContent = 'Copy';
+        }, 2000);
+      });
+    });
+    
+    return button;
+  });
+});
